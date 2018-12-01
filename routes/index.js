95ecfb63a41a2acc5d3b0d4f41d65d51197bb835
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-30 19:02:03 -0500

    WIP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express'});
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
+
+// Initial Code
+/*router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
+});*/
+
+// Changed to This...
+/*const homepageController = function(req, res) {
+  res.render('index', {title: 'Moving Shit Part 1'})
+};
+router.get('/', homepageController);*/

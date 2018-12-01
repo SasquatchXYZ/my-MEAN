8b4f94c36aec7283ec45d08028c426cc4983e049
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-01 03:29:30 -0500

    Changed up Routes settings

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+
+/* Locations Pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other Pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
-
-// Initial Code
-/*router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express'});
-});*/
-
-// Changed to This...
-/*const homepageController = function(req, res) {
-  res.render('index', {title: 'Moving Shit Part 1'})
-};
-router.get('/', homepageController);*/

dbba2374bb26a4a4547772912031ce476aac74dd
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-07 21:19:54 -0500

    WIP page109

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,14 +1,20 @@
 /* GET 'home' page*/
 module.exports.homelist = function (req, res) {
-  res.render('locations-list', {title: 'Home'})
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    }
+  })
 };
 
 /* GET 'Location info' page*/
-module.exports.locationInfo = function(req, res) {
+module.exports.locationInfo = function (req, res) {
   res.render('location-info', {title: 'Location Info'})
 };
 
 /* GET 'Add review' page*/
-module.exports.addReview = function(req, res) {
+module.exports.addReview = function (req, res) {
   res.render('location-review-form', {title: 'Add Review'})
 };

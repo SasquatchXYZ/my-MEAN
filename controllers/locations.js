0f2ddef710a66411b03b280f08bed4f06515ea1a
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-13 16:20:50 -0500

    WIP chapter 4 final views

--- controllers/locations.js
+++ controllers/locations.js
@@ -6,13 +6,14 @@ module.exports.homelist = function (req, res) {
       title: 'Loc8r',
       strapline: 'Find places to work with wifi near you!'
     },
+    sidebar: "Looking for wifi and a seat?  Loc8r helps you find places to work when out and about.  Perhaps with coffee, cake or a pint?  Let Loc8r help you find the place you're looking for.",
     locations: [{
       name: 'Starcups',
       address: '125 High Street, Reading, RG6 1PS',
       rating: 3,
       facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
       distance: '100m'
-    },{
+    }, {
       name: 'Cafe Hero',
       address: '125 High Street, Reading, RG6 1PS',
       rating: 4,
@@ -35,5 +36,8 @@ module.exports.locationInfo = function (req, res) {
 
 /* GET 'Add review' page*/
 module.exports.addReview = function (req, res) {
-  res.render('location-review-form', {title: 'Add Review'})
+  res.render('location-review-form', {
+    title: 'Review Starcups on Loc8r',
+    pageHeader: {title: 'Review Starcups'}
+  })
 };

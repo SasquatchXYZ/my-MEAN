546c0f400f895609b0b35b9c7da9075fafbd10c8
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-14 04:01:37 -0500

    establishing mongodb connections

--- models/db.js
+++ models/db.js
@@ -1,4 +1,61 @@
 const mongoose = require('mongoose');
 
 const dbURI = 'mongodb://localhost/Loc8r';
-mongoose.connect(dbURI);
+mongoose.connect(dbURI, {useNewUrlParser: true});
+
+const readLine = require('readline');
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on('SIGINT', function () {
+    process.emit('SIGINT')
+  });
+  rl.on('SIGUSR2', function () {
+    process.emit('SIGUSR2')
+  });
+  rl.on('SIGTERM', function () {
+    process.emit('SIGTERM')
+  })
+}
+
+mongoose.connection.on('connected', function () {
+  console.log(`Mongoose connected to ${dbURI}`)
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log(`Mongoose connection error: ${err}`)
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log(`Mongoose Disconnected`)
+});
+
+const gracefulShutdown = function (msg, cb) {
+  mongoose.connection.close(function() {
+    console.log(`Mongoose Disconnected through: ${msg}`);
+    cb();
+  })
+};
+
+// For Nodemon Restart
+process.once('SIGUSR2', function() {
+  gracefulShutdown('Nodemon Restart', function() {
+    process.kill(process.pid, 'SIGUSR2')
+  })
+});
+
+// For App Termination
+process.on('SIGINT', function() {
+  gracefulShutdown('App Termination', function() {
+    process.exit(0)
+  })
+});
+
+// For Heroku App Termination
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku App Shutdown', function() {
+    process.exit(0)
+  })
+});

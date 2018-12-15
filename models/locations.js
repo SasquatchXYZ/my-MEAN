52242db6c3319492b5043011c19d4aa7cc09179d
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-14 19:30:03 -0500

    defined schema

--- models/locations.js
+++ models/locations.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
+const reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
+  reviewText: String,
+  createdOn: {
+    type: Date,
+    "default": Date.now
+  }
+});
+
+const openingTimeSchema = new mongoose.Schema({
+  days: {
+    type: String,
+    required: true
+  },
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true
+  }
+});
+
 const locationSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -12,5 +40,11 @@ const locationSchema = new mongoose.Schema({
     min: 0,
     max: 5
   },
-  facilities: [String]
+  facilities: [String],
+  coords: {
+    type: [Number],
+    index: '2dsphere'
+  },
+  openingTimes: [openingTimeSchema],
+  reviews: [reviewSchema]
 });

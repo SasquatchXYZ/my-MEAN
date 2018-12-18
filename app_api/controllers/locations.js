97b102d8b90e58f46d854a55664b61c564cfbd2a
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-17 21:53:01 -0500

    working on API

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,28 +1,47 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
-module.exports.locationsCreate = function(req, res){
+module.exports.locationsCreate = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
-module.exports.locationsListByDistance = function(req, res){
+module.exports.locationsListByDistance = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
-module.exports.locationsReadOne = function(req, res){
+module.exports.locationsReadOne = function (req, res) {
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec(function (err, location) {
+        if (!location) {
+          sendJsonResponse(res, 404, {
+            'message': 'LocationID Not Found.'
+          });
+          return
+        } else if (err) {
+          sendJsonResponse(res, 404, err);
+          return
+        }
+        sendJsonResponse(res, 200, location)
+      })
+  } else {
+    sendJsonResponse(res, 404, {
+      "message": "No LocationID In Request."
+    })
+  }
+};
+
+module.exports.locationsUpdateOne = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
-module.exports.locationsUpdateOne = function(req, res){
-  sendJsonResponse(res, 200, {'status': 'success'})
-};
-
-module.exports.locationsDeleteOne = function(req, res){
+module.exports.locationsDeleteOne = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
 
-const sendJsonResponse = function(res, status, content) {
+const sendJsonResponse = function (res, status, content) {
   res.status(status)
     .json(content)
 };
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,24 +1,65 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
-module.exports.reviewsCreate = function(req, res) {
+module.exports.reviewsCreate = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
-module.exports.reviewsReadOne = function(req, res) {
+module.exports.reviewsReadOne = function (req, res) {
+  if (req.params && req.params.locationid && req.params.reviewid) {
+    Loc
+      .findById(req.params.locationid)
+      .select('name reviews')
+      .exec(function (err, location) {
+        let response, review;
+        if (!location) {
+          sendJsonResponse(res, 404, {
+            'message': 'LocationID Not Found.'
+          });
+          return
+        } else if (err) {
+          sendJsonResponse(res, 404, err);
+          return
+        }
+        if (location.reviews && location.reviews.length > 0) {
+          review = location.reviews.id(req.params.reviewid);
+          if (!review) {
+            sendJsonResponse(res, 404, {
+              'message': 'ReviewID Not Found.'
+            })
+          } else {
+            response = {
+              location: {
+                name: location.name,
+                id: req.params.locationid
+              },
+              review: review
+            };
+            sendJsonResponse(res, 200, {'status': 'success'})
+          }
+        } else {
+          sendJsonResponse(res, 404, {
+            'message': 'No Reviews Found'
+          })
+        }
+      })
+  } else {
+    sendJsonResponse(res, 404, {
+      'message': 'Not Found: LocationID and ReviewID are both required'
+    })
+  }
+};
+
+module.exports.reviewsUpdateOne = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
-module.exports.reviewsUpdateOne = function(req, res) {
-  sendJsonResponse(res, 200, {'status': 'success'})
-};
-
-module.exports.reviewsDeleteOne = function(req, res) {
+module.exports.reviewsDeleteOne = function (req, res) {
   sendJsonResponse(res, 200, {'status': 'success'})
 };
 
 
-const sendJsonResponse = function(res, status, content) {
+const sendJsonResponse = function (res, status, content) {
   res.status(status)
     .json(content)
 };
